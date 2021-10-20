5115b2fc61cd9ffd550c78c1d9d669e8cdff366f
alexey-polyashov <polyashofff@yandex.ru>
2021-10-21 02:14:27 +0300

    REST по продуктам

--- src/main/java/ru/polyan/onlinecart/OnlinecartApplication.java
+++ src/main/java/ru/polyan/onlinecart/OnlinecartApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@PropertySource("classpath:secret.properties")
 public class OnlinecartApplication {
 
 	public static void main(String[] args) {
--- src/main/java/ru/polyan/onlinecart/repositories/OrdersRepository.java
+++ src/main/java/ru/polyan/onlinecart/repositories/OrdersRepository.java
@@ -1,6 +1,5 @@
 package ru.polyan.onlinecart.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.polyan.onlinecart.model.Order;
